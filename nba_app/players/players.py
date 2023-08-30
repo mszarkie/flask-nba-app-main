a12c668c64040b3941d598773c33506a38c78382
Michał <120658970+mszarkie@users.noreply.github.com>
2023-08-30 22:42:08 +0200

    put, get particular player endpoints implemented

--- nba_app/players/players.py
+++ nba_app/players/players.py
@@ -4,7 +4,7 @@ from webargs.flaskparser import use_args
 from app import db
 from nba_app.players import blp
 from nba_app.utils import validate_content_type, get_schema_args, apply_order, apply_filter, get_pagination
-from nba_app.models import Player, PlayerSchema, players_schema
+from nba_app.models import Player, PlayerSchema, players_schema, Team, teams_schema
 
 
 @blp.route('/players', methods=['GET'])
@@ -32,3 +32,64 @@ def get_player(player_id: int):
         'success': True,
         'data': players_schema.dump(player)
     })
+
+
+@blp.route('/player/<int:player_id>', methods=['PUT'])
+@validate_content_type
+@use_args(players_schema, error_status_code=400)
+def update_player(args: dict, player_id: int):
+    player = Player.query.get_or_404(player_id, description=f'Player with id {player_id} not found')
+
+    player.first_name = args['first_name']
+    player.last_name = args['last_name']
+    player.birth_date = args['birth_date']
+    player.position = args['position']
+
+    db.session.commit()
+
+    return jsonify({
+        'success': True,
+        'data': players_schema.dump(player)
+    })
+
+
+@blp.route('/player/<int:player_id>', methods=['DELETE'])
+def delete_player(player_id: int):
+    player = Player.query.get_or_404(player_id, description=f'Player with id {player_id} not found')
+
+    db.session.delete(player)
+    db.session.commit()
+
+    return jsonify({
+        'success': True,
+        'data': f'Player with id {player_id} has been deleted'
+    })
+
+
+@blp.route('/teams/<int:team_id>/players', methods=['GET'])
+def get_all_team_players(team_id: int):
+    Team.query.get_or_404(team_id, description=f'Team with id {team_id} not found')
+    players = Player.query.filter(Player.team_id == team_id).all()
+
+    persons = PlayerSchema(many=True, exclude=['team']).dump(players)
+
+    return jsonify(({
+        'success': True,
+        'data': persons,
+        'number_of_records': len(persons)
+
+    }))
+
+
+# @blp.route('/teams/<int:team_id>/players', methods=['POST'])
+# @validate_content_type
+# @use_args(PlayerSchema(exclude=['team_id']), error_status_code=400)
+# def create_player(args: dict, team_id: int):
+#     Team.query.get_or_404(team_id, description=f'Team with id {team_id} not found')
+#
+#     player = Player(team_id=team_id, **args)
+#
+#     return jsonify(({
+#         'success': True,
+#         'data': player
+#     })), 201
--- nba_app/teams/teams.py
+++ nba_app/teams/teams.py
@@ -4,7 +4,7 @@ from webargs.flaskparser import use_args
 from app import db
 from nba_app.teams import blp
 from nba_app.utils import validate_content_type, get_schema_args, apply_order, apply_filter, get_pagination
-from nba_app.models import Team, TeamSchema, teams_schema
+from nba_app.models import Team, TeamSchema, teams_schema, Player, PlayerSchema
 
 
 @blp.route('/teams', methods=['GET'])
@@ -78,3 +78,4 @@ def delete_team(team_id: int):
         'success': True,
         'data': f'Team with id {team_id} has been deleted'
     })
+
